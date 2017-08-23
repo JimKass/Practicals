f04f4f0c204d47c7548de3ef3fc3677add3c23ba
JimKass <james.kastner@my.jcu.edu.au>
2017-08-23 15:11:18 +1000

    Fix up code after review Part 2 (The reviewening)

--- prac_04/number_facts.py
+++ prac_04/number_facts.py
@@ -1,6 +1,6 @@
 
 def main():
-    numbers = [int(input("Number: ")) for i in range(0,5)]
+    numbers = [int(input("Number: ")) for i in range(5)]
     print("The first numbe is " + str(numbers[0]))
     print("The last number is " + str(numbers[-1]))
     print("The smallest number is " + str(min(numbers)))
