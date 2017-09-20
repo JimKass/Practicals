040a8ba7157d566d61d19c0bfc74a7ddc4a92956
JimKass <jimmy.kastner@gmail.com>
2017-09-20 13:19:40 +1000

    Add __init__ and __str__ functions for ProgrammingLanguage class

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -2,5 +2,16 @@
 
 """
 
+
 class ProgrammingLanguage:
-    pass
+    def __init__(self,name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name,
+                                                                           self.typing, self.reflection, self.year)
+
+
