29fba9f00837d44b2c4f856353ec584e26dd0550
JimKass <jimmy.kastner@gmail.com>
2017-09-20 13:49:56 +1000

    Add  get_age and is_vintage methods to Guitar class

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -4,13 +4,26 @@ class Guitar():
     Represents a guitar object.
     """
 
-    def __init__(self, name, year, cost):
+    def __init__(self, name, year, cost, current_year=2017):
         """
         Initialize Guitar class.
         """
         self.name = name
         self.year = year
         self.cost = cost
+        self.current_year = current_year
 
     def __str__(self):
         return "{} ({}) : {}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """
+        Returns the age of the guitar.
+        """
+        return self.current_year - self.year
+
+    def is_vintage(self):
+        """
+        Returns whether or not the guitar is over 50 years old.
+        """
+        return self.get_age() > 50
